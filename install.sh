0cb63e822ff0b9bcd43d53ec9dd0714c03792643
toby <toby.lee@foxmail.com>
2020-07-11 13:33:29 +0800

    feat: 简化代码

--- install.sh
+++ install.sh
@@ -7,13 +7,10 @@
 set -e
 
 LINUX_CONFIG_PATH=$(dirname $(readlink -f $0))
+PIP_SOURCE='-i https://pypi.douban.com/simple'
 
 ins_nvim()
 {
-	# ubuntu 18以上的版本不需要自己添加仓库
-	#sudo apt-get install -y software-properties-common
-	#sudo add-apt-repository ppa:neovim-ppa/stable
-	#sudo apt-get update
 	sudo apt-get install -y neovim
 
 	# TODO: 待测试
@@ -37,13 +34,11 @@ ins_nvim()
 
 ins_python()
 {
-	#sudo apt-get install -y python python-dev python-pip
 	sudo apt-get install -y python3 python3-dev python3-pip
 	make -C $LINUX_CONFIG_PATH install-pip
-	sudo pip3 install -U pip
-	sudo pip3 install virtualenv
+	sudo pip3 install -U pip $PIP_SOURCE
+	sudo pip3 install virtualenv $PIP_SOURCE
 	mkdir -p ~/.env
-	#virtualenv -p python ~/.env/py2
 	virtualenv -p python3 ~/.env/py3
 }
 
@@ -57,43 +52,44 @@ ins_zsh()
 
 ins_fzf()
 {
-	if [[ -e ~/.fzf ]]; then
-		(cd ~/.fzf; git pull)
-	else
-		git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
-	fi
-	sudo ~/.fzf/install
+       if [[ -e ~/.fzf  ]]; then
+               (cd ~/.fzf; git pull)
+       else
+               git clone --depth 1 https://github.com/junegunn/fzf.git ~/.fzf
+       fi
+       sudo ~/.fzf/install
 }
 
 ins_other()
 {
 	# common
 	sudo apt-get install -y vim openssh-client openssh-server lrzsz
-	# zsh plugin
-	sudo apt-get install -y autojump silversearcher-ag
+	# tools
+	sudo apt-get install -y autojump silversearcher-ag fzf
 	# nvim
 	sudo apt-get install -y exuberant-ctags
-    # 神器jq: 格式化json显示，替换python -m json.tool
-    # tig: 交互式的git
-    sudo apt-get install -y jq tig mycli
+	# 神器jq: 格式化json显示，替换python -m json.tool
+	# tig: 交互式的git
+	sudo apt-get install -y jq tig mycli
 }
 
 ins_zsh_plug()
 {
-    ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
+	ZSH_CUSTOM=${ZSH_CUSTOM:-~/.oh-my-zsh/custom}
 	# 该函数必须在zsh安装完后安装，由于该函数会生成.oh-my-zsh目录，导致ins_zsh无法正常安装
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git $ZSH_CUSTOM/plugins/zsh-syntax-highlighting
-    git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+	git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
 }
 
 # 用于装完系统后安装各类工具
 init()
 {
+	sudo apt update
 	ins_python
 	ins_nvim
 	make -C $LINUX_CONFIG_PATH
-	ins_fzf
 	ins_other
+	ins_tmux
 	# zsh必须在最后安装，由于它会将终端切到zsh，从而中断脚本
 	ins_zsh
 }
@@ -101,13 +97,12 @@ init()
 # 安装用于nvim的python插件
 ins_pytools()
 {
-    pip_source='-i https://pypi.douban.com/simple'
 	# for nvim
-	pip install -U pip neovim jedi flake8 pep8 pylint $pip_source
+	pip install -U pip neovim jedi flake8 pep8 pylint $PIP_SOURCE
 
 	# tools
 	#pip install thefuck pipreqs mycli alembic ipdb
-	pip install -U pipreqs ipdb ipython ranger $pip_source
+	pip install -U pipreqs ipdb ipython ranger $PIP_SOURCE
 }
 
 # 安装nvim插件配置
@@ -118,18 +113,18 @@ ins_nvim_plug_conf()
 	diy_py_snip=$LINUX_CONFIG_PATH/nvim/python.snippets
 	if [[ -z $(grep "# DIY" $py_snip) ]]; then
 		cp $py_snip $py_snip_bak
-    else
-        cp $py_snip_bak $py_snip
+	else
+		cp $py_snip_bak $py_snip
 	fi
-    cat $diy_py_snip >> $py_snip
+	cat $diy_py_snip >> $py_snip
 }
 
 
 # 安装tmux
 ins_tmux()
 {
-    sudo apt-get install -y tmux
-    git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
+	sudo apt-get install -y tmux
+	git clone https://github.com/tmux-plugins/tpm ~/.tmux/plugins/tpm
 }
 
 help()
@@ -137,13 +132,12 @@ help()
 	cat << EOF
 Usage: ./install.sh [OPT]
 OPT:
-	init:                        执行ins_python, ins_nvim, ins_zsh, ins_fzf, ins_other, make
+	init:                        执行ins_python, ins_nvim, ins_zsh, ins_other, make
 	|- ins_python:               安装python以及虚拟环境
-	|- ins_zsh:                  安装zsh
-	|- ins_tmux:                 安装tmux
-	|- ins_fzf:                  安装fzf
 	|- ins_nvim:                 安装nvim以及相关插件
 	   |- ins_nvim_plug_conf:    安装nvim插件配置
+	|- ins_tmux:                 安装tmux
+	|- ins_zsh:                  安装zsh
 
     # 以下工具需以上工具安装后自行安装
 	ins_zsh_plug:       安装zsh的脚本，必须在安装zsh后执行，否则会阻碍oh-my-zsh的安装
